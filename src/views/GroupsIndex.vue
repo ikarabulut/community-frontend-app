d548818893a0f20ecc46570963e4fb2699fc2537
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-16 12:46:18 -0600

    adds GroupsIndex

---
+++ src/views/GroupsIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="groups-index">
+    <h1>All Groups</h1>
+    <div v-for="group in groups" v-bind:key="group.id">
+      <h2>{{ group.name }}</h2>
+      <p>Mission Statement: {{ group.mission_statement }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      groups: [],
+    };
+  },
+  created: function () {
+    this.indexGroups();
+  },
+  methods: {
+    indexGroups: function () {
+      axios.get("/groups").then((response) => {
+        console.log("groups index", response);
+        this.groups = response.data;
+      });
+    },
+  },
+};
+</script>
