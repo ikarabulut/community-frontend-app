03bae20360b26568dfebe4e02f35ab2c7c5b4c06
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-20 12:32:23 -0600

    adds html theme to /public and updates app.vue headers

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,167 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/groups">All Groups</router-link>
-      |
-      <router-link to="/group/new">Create Group</router-link>
-      |
-      <router-link to="/events">All Events</router-link>
-      |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/login">Login</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
+  <!-- header -->
+  <header>
+    <!-- nav -->
+    <nav class="navbar navbar-default nav-sec navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo" /></a>
+        </div>
+        <!-- / navbar-header -->
+        <div class="secondary-nav">
+          <a href="login-register.html" class="my-account space-right"><i class="fa fa-user"></i></a>
+          <a href="shopping-cart.html" class="shopping-cart">
+            <i class="fa fa-shopping-cart"></i>
+            <span class="cart-badge">2</span>
+          </a>
+        </div>
+        <div class="navbar-collapse collapse text-center">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a href="index.html"><span>HOME</span></a>
+            </li>
+            <li>
+              <a href="about.html"><span>ABOUT</span></a>
+            </li>
+            <li class="dropdown">
+              <a
+                href="#"
+                class="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <span>BLOG</span>
+                <span class="dropdown-icon"></span>
+              </a>
+              <ul class="dropdown-menu animated zoomIn fast">
+                <li>
+                  <a href="blog.html"><span>BLOG FULLWIDTH</span></a>
+                </li>
+                <li>
+                  <a href="blog-masonry.html"><span>BLOG MASONRY</span></a>
+                </li>
+                <li>
+                  <a href="blog-sidebar.html"><span>BLOG SIDEBAR</span></a>
+                </li>
+                <li>
+                  <a href="single-post-full.html"><span>POST FULLWIDTH</span></a>
+                </li>
+                <li>
+                  <a href="single-post.html"><span>POST SIDEBAR</span></a>
+                </li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a
+                href="#"
+                class="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <span>SHOP</span>
+                <span class="dropdown-icon"></span>
+              </a>
+              <ul class="dropdown-menu animated zoomIn fast">
+                <li>
+                  <a href="shop.html"><span>FULL WIDTH</span></a>
+                </li>
+                <li>
+                  <a href="shop-right.html"><span>RIGHT SIDEBAR</span></a>
+                </li>
+                <li>
+                  <a href="shop-left.html"><span>LEFT SIDEBAR</span></a>
+                </li>
+                <li>
+                  <a href="shop-masonry.html"><span>MASONRY</span></a>
+                </li>
+                <li>
+                  <a href="single-product.html"><span>SINGLE PRODUCT</span></a>
+                </li>
+                <li>
+                  <a href="single-product2.html"><span>SINGLE PRODUCT 2</span></a>
+                </li>
+                <li>
+                  <a href="single-product3.html"><span>SINGLE PRODUCT 3</span></a>
+                </li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a
+                href="#"
+                class="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <span>PAGES</span>
+                <span class="dropdown-icon"></span>
+              </a>
+              <ul class="dropdown-menu animated zoomIn fast">
+                <li>
+                  <a href="faq.html"><span>FAQ</span></a>
+                </li>
+                <li>
+                  <a href="shopping-cart.html"><span>SHOPPING CART</span></a>
+                </li>
+                <li>
+                  <a href="login-register.html"><span>LOGIN / REGISTER</span></a>
+                </li>
+                <li>
+                  <a href="my-account.html"><span>MY ACCOUNT</span></a>
+                </li>
+                <li>
+                  <a href="checkout.html"><span>CHECKOUT</span></a>
+                </li>
+                <li>
+                  <a href="404.html"><span>404 PAGE</span></a>
+                </li>
+                <li>
+                  <a href="components.html"><span>COMPONENTS</span></a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="contact.html"><span>CONTACT</span></a>
+            </li>
+          </ul>
+        </div>
+        <!--/ nav-collapse -->
+      </div>
+      <!-- / container -->
+    </nav>
+    <!-- / nav -->
+
+    <!-- header-banner -->
+    <div id="header-banner" class="demo-1">
+      <div class="banner-content text-center">
+        <div class="banner-border">
+          <div class="banner-info">
+            <h1>Minimal Shop</h1>
+            <p>Minimal Fashion Shop</p>
+          </div>
+          <!-- / banner-info -->
+        </div>
+        <!-- / banner-border -->
+      </div>
+      <!-- / banner-content -->
     </div>
-    <router-view />
-  </div>
+    <!-- / header-banner -->
+  </header>
+  <!-- / header -->
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
