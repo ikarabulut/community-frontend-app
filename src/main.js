06abc1051e37efb90079aa66d51019cd24be1c28
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-28 11:24:56 -0600

    fixes development bug in main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://fast-reef-47047.herokuapp.com" : "/";
-
-axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://fast-reef-47047.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
